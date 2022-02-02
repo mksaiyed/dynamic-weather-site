cdf425f5c4ed30fa8efe09c89fc1d9fc3d1f1c89
mksaiyed <mo.kaifbapu@gec28.ac.in>
2022-02-02 18:18:01 +0530

    UI completed with Fetching data

--- script.js
+++ script.js
@@ -31,3 +31,114 @@ document.querySelector(".dd").innerText = dd;
 document.querySelector(".mm").innerText = monthNames[mm];
 document.querySelector(".yy").innerText = yy;
 document.querySelector(".day").innerText = dayNames[day];
+
+const cityNameList = document.querySelector(".place-list");
+const cardContainer = document.querySelector(".card-container");
+
+(async function () {
+  // Fetch City Names
+  const cityNames = await fetch(
+    "https://raw.githubusercontent.com/Dipen-Dedania/static-data/main/india-popular-city.json"
+  ).then((response) => response.json());
+
+  // Add Cities in HTML
+  for (let i = 0; i < cityNames.city.length; i++) {
+    const div = document.createElement("div");
+    div.addEventListener("click", updateCity(this));
+    div.innerText = cityNames.city[i].name;
+    cityNameList.append(div);
+  }
+
+  // Fetch Card Details
+  const cards = await fetch(
+    "https://raw.githubusercontent.com/Dipen-Dedania/static-data/main/make-your-trip-package.json"
+  ).then((response) => response.json());
+
+  // Add Card in HTML
+  for (let i = 0; i < cards.length; i++) {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    const bookmark = cards[i].isBookmark;
+    div.innerHTML = `
+    <i class="${bookmark === true ? "fas" : "far"} fa-bookmark bookmark"></i>
+    <!-- Card Header -->
+    <div>
+      <p class="card-heading">${cards[i].cityName}</p>
+      <p class="card-date">${cards[i].tourDate}</p>
+      <p class="card-category silver">${cards[i].category}</p>
+    </div>
+
+    <!-- Card Temperature -->
+    <div class="card-temperature">
+      <p class="label">Average Temperature</p>
+      <div class="line"></div>
+      <p class="temperature-text">
+        + ${cards[i].temp}&deg;
+        <i class="fas fa-sun sun"></i>
+      </p>
+    </div>
+
+    <!-- Image -->
+    <img
+      class="card-image"
+      src="${cards[i].cityImg}"
+    />
+
+    <!-- Card Bottom prize & button -->
+    <div class="card-bottom">
+      <div>
+        <p class="label silver">Total Price</p>
+        <p class="price">${cards[i].price}</p>
+      </div>
+      <button class="explore-btn">Explore</button>
+    </div>`;
+    cardContainer.append(div);
+  }
+})();
+
+// Show events on click
+const listBox = document.querySelectorAll(".list-box");
+const placeList = document.querySelector(".place-list");
+const dayList = document.querySelector(".day-list");
+const dateList = document.querySelector(".date-list");
+window.addEventListener("click", (e) => {
+  // console.log(e.target.parentNode.matches(".place-box"));
+  // console.log(e.target.parentNode.classList.contains("place-box"));
+  if (
+    e.target.classList.contains("place-box") ||
+    e.target.parentNode.classList.contains("place-box")
+  ) {
+    placeList.style.display === "block"
+      ? (placeList.style.display = "none")
+      : (placeList.style.display = "block");
+    dayList.style.display = "none";
+    dateList.style.display = "none";
+  } else if (
+    e.target.classList.contains("day-box") ||
+    e.target.parentNode.classList.contains("day-box")
+  ) {
+    dayList.style.display === "block"
+      ? (dayList.style.display = "none")
+      : (dayList.style.display = "block");
+    placeList.style.display = "none";
+    dateList.style.display = "none";
+  } else if (
+    e.target.classList.contains("date-box") ||
+    e.target.parentNode.classList.contains("date-box")
+  ) {
+    dateList.style.display === "block"
+      ? (dateList.style.display = "none")
+      : (dateList.style.display = "block");
+    placeList.style.display = "none";
+    dayList.style.display = "none";
+  } else {
+    placeList.style.display = "none";
+    dayList.style.display = "none";
+    dateList.style.display = "none";
+  }
+});
+
+function updateCity(e) {
+  console.log("Hii");
+  console.log(e);
+}
